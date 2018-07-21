0308043a7db203f37865bb785f672b1bfa365857
akshat <akshatbhargavadeepa@gmail.com>
2018-07-22 02:47:02 +0530

    made login and register endpoints and restructured.

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,35 +1,14 @@
 import { Router } from 'express';
-import { hashSync } from 'bcryptjs';
-import { sign as jwtSign, verify as jwtVerify } from 'jsonwebtoken';
-import { toRes } from './../../lib/util';
 import users from './users';
 
 export default ({ config, db }) => {
   let api = Router();
 
-  // perhaps expose some API metadata at the root
-  api.post('/login', (req, res) => {
-    const token = req.headers.authorization;
-    if (!token) toRes(res, 401)({ message: 'No token found!' }, null);
-    jwtVerify(token, config.jwt_secret, (err, decoded) => {
-      if (err) return toRes(res, 500)({ message: 'Invalid token' }, null);
-      toRes(res)(null, { decoded });
-    });
-  });
+  // login endpoint
+  api.post('/login', (req, res) => users({req, res, config, db}).login());
 
-  api.post('/register', (req, res) => {
-    const hashedPassword = hashSync(req.body.password, 8);
-    const user = {
-      name: 'akshat',
-      email: 'a@b.com',
-      password: '123456'
-    };
-    console.log(config.jwt_secret)
-    const token = jwtSign(user, config.jwt_secret, {
-      expiresIn: "30d"
-    });
-    toRes(res)(null, { token });
-  });
+  // register a user endpoint
+  api.post('/register', (req, res) => users({req, res, config, db}).register());
 
   return api;
 }
---
+++ src/api/users/users.js
@@ -0,0 +1,43 @@
+import UserModel from '../../models/users';
+import { hashSync, compareSync } from 'bcryptjs';
+import { sign as jwtSign } from 'jsonwebtoken';
+import { toRes, mongooseErrorHandler } from './../../lib/util';
+
+export default ({ req, res, config, db }) => ({
+
+	login() {
+		UserModel(db).findOne({
+			email: req.body.email
+		}, (err, user) => {
+			if (err) return toRes(res, 500)(mongooseErrorHandler(err));
+			if (!user) return toRes(res, 404)({ message: 'No User Found!' });
+			const passwordIsValid = compareSync(req.body.password, user.password);
+			if (!passwordIsValid) return toRes(res, 401)({ message: 'Invalid credentials!' });
+			const token = jwtSign({
+				id: user._id,
+				name: user.name,
+				email: user.email
+			}, config.jwt_secret, { expiresIn: 86400 });
+			toRes(res)(null, { token });
+		});
+	},
+
+	register() {
+		const hashedPassword = hashSync(req.body.password, 8);
+		UserModel(db).create({
+			email: req.body.email,
+			password: hashedPassword,
+			name: req.body.name
+		}, (err, result) => {
+			if (err) return toRes(res, 500)(mongooseErrorHandler(err));
+
+			let newUser = result.toJSON();
+			delete newUser.password;
+			const token = jwtSign(newUser, config.jwt_secret, {
+				expiresIn: "30d"
+			});
+			toRes(res)(err, { token });
+		});
+	}
+
+});
