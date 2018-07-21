e100777195eef92079ea079e476a07adb6cd8beb
akshat <akshatbhargavadeepa@gmail.com>
2018-07-22 02:43:52 +0530

    did database connection setup

--- src/db.js
+++ src/db.js
@@ -1,4 +1,22 @@
+import mongoose from 'mongoose';
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
-	callback();
-}
+	mongoose.connect('mongodb://test:test.123@ds245901.mlab.com:45901/todolist-app');
+
+	const db = mongoose.connection;
+
+	db.on('connected', () => {
+		console.log('Connected to database');
+	});
+
+	db.on('disconnected', () => {
+		console.log('Connection to database disconnected');
+	});
+
+	db.on('error', () => {
+		console.log('Error occured while connecting to database');
+	});
+
+	callback(db);
+
+};
