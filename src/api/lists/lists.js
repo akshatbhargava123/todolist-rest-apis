9bd4d462d8de798c57294076686fe5c65506e303
akshat <akshatbhargavadeepa@gmail.com>
2018-07-22 03:33:46 +0530

    fixed minor bug

--- src/api/lists/lists.js
+++ src/api/lists/lists.js
@@ -5,12 +5,12 @@ export default ({ req, res, config, db }) => ({
 
 	create() {
 		ListModel(db).create({
-			userId: req.user.id,
+			userId: req.user._id,
 			...req.body
 		}, (err, result) => {
 			if (err) return toRes(res, 500)(mongooseErrorHandler(err));
 			toRes(res)(null, { savedList: result });
-		})
+		});
 	},
 
 	fetchAll() {
