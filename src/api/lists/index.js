2635ac7b42a41f6df6b7c9e16478d50be9323f77
akshat <akshatbhargavadeepa@gmail.com>
2018-07-22 13:18:53 +0530

    created api docs

--- src/api/lists/index.js
+++ src/api/lists/index.js
@@ -1,15 +1,65 @@
 import { Router } from 'express';
 import lists from './lists';
 
+/**
+ * @apiDefine AuthHeader
+ * @apiHeader (Headers) {String} authorization Authorization token in format: 'Bearer {token}'
+ */
+
 export default ({ config, db }) => {
 	let api = Router();
 
-	// get all lists
+  /**
+   * @api {get} /lists Fetch all lists
+   * @apiGroup Lists
+   * 
+	 * @apiUse AuthHeader
+	 * 
+   * @apiSuccess {Boolean} error false
+   * @apiSuccess {Array} lists Array of lists
+   */
+
 	api.get('/', (req, res) => lists({ req, res, config, db }).fetchAll());
+
+  /**
+   * @api {post} /lists Create a new list
+   * @apiGroup Lists
+   * 
+	 * @apiUse AuthHeader
+	 * 
+	 * @apiParam {Object[]} items Array of item
+	 * @apiParam {String} -.text Text of task
+	 * @apiParam {Boolean} -.isCompleted Represents if task is completed (default: false)
+	 *
+   * @apiSuccess {Boolean} error false
+   * @apiSuccess {Object} savedList Newly created list
+   */
 
 	api.post('/', (req, res) => lists({ req, res, config, db }).create());
 
+  /**
+   * @api {put} /lists/:id Update a list
+   * @apiGroup Lists
+   * 
+	 * @apiUse AuthHeader
+	 * 
+	 * @apiParam {Object[]} items Array of item
+	 * @apiParam {String} -.text Text of task
+	 * @apiParam {Boolean} -.isCompleted Represents if task is completed
+	 *
+   * @apiSuccess {Boolean} error false
+   */
+
 	api.put('/:id', (req, res) => lists({ req, res, config, db }).updateList());
+
+  /**
+   * @api {delete} /lists/:id Delete a list
+   * @apiGroup Lists
+   * 
+	 * @apiUse AuthHeader
+	 *
+   * @apiSuccess {Boolean} error false
+   */
 
 	api.delete('/:id', (req, res) => lists({ req, res, config, db }).deleteList());
 
--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,10 +4,32 @@ import users from './users';
 export default ({ config, db }) => {
   let api = Router();
 
-  // login endpoint
+  /**
+   * @api {post} /users/login Login
+   * @apiGroup User
+   * 
+   * @apiParam {String} email User's email
+   * @apiParam {String} password User's password
+   * 
+   * @apiSuccess {Boolean} error false
+   * @apiSuccess {String} token Authorization Token
+   */
+
   api.post('/login', (req, res) => users({req, res, config, db}).login());
 
-  // register a user endpoint
+
+  /**
+   * @api {post} /users/register Register
+   * @apiGroup User
+   * 
+   * @apiParam {String} name User's name
+   * @apiParam {String} email Email to be registered
+   * @apiParam {String} password Password of new account
+   * 
+   * @apiSuccess {Boolean} error false
+   * @apiSuccess {String} token Authorization Token
+   */
+
   api.post('/register', (req, res) => users({req, res, config, db}).register());
 
   return api;
